90a4ca4f67cbdd60dc24953aa3c891ec4050e9eb
Nguyễn Văn Đạt 108398 <dat108398@donga.edu.vn>
2026-05-22 20:42:09 +0700

    Delete src directory

--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('../index');
-const PORT = 2006;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-    console.log(`Server đang chạy tại: http://localhost:${PORT}`);
-});
